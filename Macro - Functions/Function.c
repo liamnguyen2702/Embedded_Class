a061ed72c15627229ba55ad8a59e93ff4adabf28
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-05-29 15:33:29 +0700

    Update and rename Inline_function.c to Function.c

---
+++ Macro - Functions/Function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int SUM (int a, int b)
+{
+  return a + b;
+}
+
+int main()
+{
+  printf("Tong a va b: %d\n", SUM (5, 6));
+  
+  printf("Tong a va b: %d\n", SUM (9, 10));
+
+  return 0;
+}
