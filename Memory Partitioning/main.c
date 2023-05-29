30322758614fbf87e20d8a0b5df4cab4c5c3b3db
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-05-29 14:07:35 +0700

    Update main.c

--- Memory Partitioning/main.c
+++ Memory Partitioning/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+char arr[] = "Hello"; // DS partitioning (global array)
+
+char *arr_b = "Hello"; // text partitioning (pointer)
+
+int main()
+{
+    arr_b[0] = "A";
+    
+    printf("Test: %s\n", arr); // Display: Hello
+    
+    printf("Test: %s\n", arr_b); // Error because DS partitioning can not write
+  
+    return 0;
+}
