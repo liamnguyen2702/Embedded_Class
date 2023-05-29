c4846a3a7df83e31c0a7eb7115e112110135e785
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-05-29 16:28:40 +0700

    Update main.c

--- Static Variable/main.c
+++ Static Variable/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+void test()
+{
+  static int a = 10;
+  printf("a = %d\n", a);
+  a++;
+}
+
+int main()
+{
+  test(); // 10
+  
+  test(); // 11
+  
+  test(); // 12
+
+  return 0;
+}
 
