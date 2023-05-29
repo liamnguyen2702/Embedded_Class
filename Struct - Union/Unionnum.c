b74931b207f62996f7ff7df2e73bd772a6858819
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-05-29 16:21:24 +0700

    Create Unionnum.c

---
+++ Struct - Union/Unionnum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef union
+{
+  uint8_t test1[6]; 
+  uint8_t test2[3];
+}typeData;
+
+int main()
+{
+  typeData data;
+  
+  for (int i = 0; i < 6; i++)
+  {
+    data.test1[i] = i;
+  }
+  
+   for (int i = 0; i < 3; i++)
+  {
+    data.test2[i] = 2 * i;
+  }
+  return 0;
+}
