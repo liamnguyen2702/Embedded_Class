d36c22853867bc5bcb755faaada663abec5dd98c
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-05-29 11:01:18 +0700

    Update main.c

--- Compiler/main.c
+++ Compiler/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+#define MAX 10
+
+#define SUM(a,b) a + b
+
+int main()
+{
+    printf("MAX = %d\n", MAX);
+  
+    printf("Tong a va b = %d\n", SUM (7,8));
+  
+    return 0; 
+}
+  
 
