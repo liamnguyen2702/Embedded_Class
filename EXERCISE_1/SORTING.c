0cb3ed3206a3d121a8725092daf03cc3d445f272
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-05-30 15:58:22 +0700

    Update SORTING.c

--- EXERCISE_1/SORTING.c
+++ EXERCISE_1/SORTING.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort(int arr[], int n)
+{
+    int i, j, min_p;
+    for (i = 0; i < n-1; i++)
+    {
+        min_p = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_p])
+                min_p = j;
+        swap(&arr[min_p], &arr[i]);
+    }
+}
+
+void print(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+   int arr[] = {4, 5, 7, 2, 9, 8, 3, 7, 4, 8, 5, 9, 6, 7, 4, 6, 4, 8, 5, 6, 8, 5, 6, 4};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   sort(arr, n);
+
+   print(arr, n);
+   return 0;
+}
