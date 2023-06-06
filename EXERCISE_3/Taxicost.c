7962cac906abbaa01240c58960446d602999e535
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-06-07 02:46:18 +0700

    Update Taxicost.c

--- EXERCISE_3/Taxicost.c
+++ EXERCISE_3/Taxicost.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+float calculateCost(float km)
+{
+    float cost;
+
+    if (km <= 1)
+        cost = 14;
+    else if (km <= 30)
+        cost = (km - 1) * 10 + 14;
+    else
+        cost = 14 + 29 * 10 + (km - 30) * 8;
+    return cost;
+}
+
+int main()
+{
+    float km = 1;
+
+    printf("Cost = %.2f thousand dong\n", calculateCost(km));
+
+    return 0;
+}
