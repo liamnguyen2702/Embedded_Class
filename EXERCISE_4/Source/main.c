2dd49aee8e6cb0dbbf5d8922293438fba6166e2f
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-06-12 07:34:10 +0700

    Update main.c

--- EXERCISE_4/Source/main.c
+++ EXERCISE_4/Source/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
 
+int main() {
+    char expression[MAX_SIZE];
+
+    printf("Enter an infix expression: ");
+    fgets(expression, sizeof(expression), stdin);
+
+    float result = evaluatePostfix(processExpression(expression));
+
+    printf("The result is: %f\n", result);
+
+    return 0;
+}
