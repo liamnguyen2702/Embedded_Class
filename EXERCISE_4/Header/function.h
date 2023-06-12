c3641d0a7d922c6a989ce7eb5645de56512d05ad
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-06-12 07:33:12 +0700

    Update function.h

--- EXERCISE_4/Header/function.h
+++ EXERCISE_4/Header/function.h
@@ -1,1 +1,32 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#define MAX_SIZE 100
+
+typedef struct {
+    char data[MAX_SIZE];
+    int top;
+} Stack;
+
+typedef struct {
+    char data[MAX_SIZE];
+    int front;
+    int rear;
+} Queue;
+
+void initStack(Stack *stack);
+int isStackEmpty(Stack *stack);
+int isStackFull(Stack *stack);
+void push(Stack *stack, char value);
+char pop(Stack *stack);
+void initQueue(Queue *queue);
+int isQueueEmpty(Queue *queue);
+int isQueueFull(Queue *queue);
+void enqueue(Queue *queue, char value);
+char dequeue(Queue *queue);
+int getOperatorPriority(char opChar);
+char* processExpression(char *expression);
+float evaluatePostfix(char *expression);
+
+#endif
 
