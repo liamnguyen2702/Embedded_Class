c73024d7e750839b05bfdb90322282a8be1afa72
liamnguyen2702 <133777741+liamnguyen2702@users.noreply.github.com>
2023-06-12 08:41:58 +0700

    Update main.c

--- EXERCISE_4/Equation/Source/main.c
+++ EXERCISE_4/Equation/Source/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "function.h"
 
+int main()
+{
+    char arr[100];
+
+    printf("Enter your equation: ");
+    fgets(arr, sizeof(arr), stdin);
+
+    check(arr);
+
+    return 0;
+}
